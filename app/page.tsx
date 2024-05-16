92d34f4498818aa36bda77bc3a0cb9dd2d9e8be7
Victor <vi7org@hotmail.com>
2024-05-16 18:13:10 -0400

    Add link to about page, minor CSS changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import Link from "next/link";
+
 export default function Home() {
-  return (
-      <main>
-        <img src="/logo.png" alt="logo" />
-        <h1>Welcome to Tech Tinker!</h1>
-        <p>Placeholder</p>
-      </main>
-  );
+    return (
+        <main>
+            <img id={"index-logo"} src="/logo.png" alt="a robot tinkering"/>
+            <h1>Welcome to Tech Tinker!</h1>
+            <p><Link href={"/about"}>About</Link></p>
+        </main>
+    );
 }
