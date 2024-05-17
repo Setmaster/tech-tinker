c4ef9d6cf6034e924821f8d2a4ddf39e3699e103
Victor <vi7org@hotmail.com>
2024-05-17 09:44:05 -0400

    Add dummy image assets

--- components/header.tsx
+++ components/header.tsx
@@ -1,7 +1,7 @@
 export default function Header(){
     return (
         <>
-            <img id={"index-logo"} src="/logo.png" alt="a robot tinkering"/>
+            <img id={"index-logo"} src="/images/logo.png" alt="a robot tinkering"/>
             <h1>Welcome to Tech Tinker!</h1>
         </>
     );
